c4f4b8c5c7bc594ba1aa88fa3bbd6ca1f2197a54
Kittaporn <kittaporn.j@ku.th>
2018-01-26 01:28:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/narathorn/apartmentkunparparradnarjan/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.example.narathorn.apartmentkunparparradnarjan;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText roomNumber,passWord;
+    Button signInBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("INFORMATION");
+        roomNumber = (EditText)findViewById(R.id.roomNumber);
+        passWord = (EditText) findViewById(R.id.passWord);
+        signInBtn = (Button) findViewById(R.id.signInBtn);
+        SharedPreferences shared = getSharedPreferences("login_pref",Context.MODE_PRIVATE);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!(roomNumber.getText().equals(""))) {
+                    myRef.child(roomNumber.getText().toString()).child("PASSWORD").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            String password = dataSnapshot.getValue(String.class);
+                            if(password.equals(passWord.getText().toString())){
+                                //Intent i = new Intent(LoginActivity.this,)
+                                Toast.makeText(LoginActivity.this,"Thank you for loging in",Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(LoginActivity.this,"Your Password Is Not Correct",Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // Failed to read value
+                        }
+                    });
+                }else{
+                    Toast.makeText(LoginActivity.this,"5555",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
